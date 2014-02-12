4d814a1e90697941d1d4b408209fea57cf9ef303
James Long <longster@gmail.com>
2014-02-12 12:59:02 -0500

    fix locale links

--- _plugins/locale.rb
+++ _plugins/locale.rb
@@ -3,14 +3,16 @@
 module Jekyll
   module LocaleFilter
     
-    def locale_buttons(url)
+    def locale_buttons(url, baseurl)
       if url.start_with?('/cn/')
-        en_url = url.sub(/^\/cn\//, '/')
+        en_url = File.join(baseurl, url.sub(/^\/cn\//, '/'))
+        cn_url = File.join(baseurl, url)
         '<a href="' + en_url + '" class="btn btn-default">English</a>' +
-          '<a href="' + url + '" class="btn btn-success">中国的</a>'
+          '<a href="' + cn_url + '" class="btn btn-success">中国的</a>'
       else
-        cn_url = url.sub(/^\//, '/cn/')
-        '<a href="' + url + '" class="btn btn-success">English</a>' +
+        en_url = File.join(baseurl, url)
+        cn_url = File.join(baseurl, url.sub(/^\//, '/cn/'))
+        '<a href="' + en_url + '" class="btn btn-success">English</a>' +
           '<a href="' + cn_url + '" class="btn btn-default">中国的</a>'
       end
     end
